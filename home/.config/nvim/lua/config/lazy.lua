99cc75be44453613a51525cce5757e396a80d0f5
Banghao Chi <banghao2@illinois.edu>
2026-08-22 20:15:30 -0500

    chore(nvim): stop lazy.nvim polling for plugin updates

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -32,10 +32,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- plugin versions are pinned by lazy-lock.json
   performance = {
     rtp = {
       -- disable some rtp plugins
